3d4793eb0666fbd6868596ceafa6ee711b0365f0
AshutoshJadhav01 <x22183868@student.ncirl.ie>
2025-05-25 17:52:13 +0100

    Added testing post api

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const db = require("./db");
+const bunyan = require("bunyan");
+let log = bunyan.createLogger({ name: "tracker-index.js" });
 // const { auth } = require("express-openid-connect");
 // const { requiresAuth } = require("express-openid-connect");
 
@@ -17,11 +19,17 @@ const db = require("./db");
 // app.use(auth(config));
 
 app.get("/", (req, res) => {
-  console.log("hey");
   res.send("Hey welcome to expense tracker");
   //   res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
 });
 
+app.post("/add_transaction", (req, res) => {
+  log.info("add_transaction");
+  res.status(200).json({
+    status: "success",
+    message: "Transaction received successfully",
+  });
+});
 // app.get("/profile", requiresAuth(), (req, res) => {
 //   res.send(JSON.stringify(req.oidc.user));
 // });
